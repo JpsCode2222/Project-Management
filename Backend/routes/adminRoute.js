a3decfc4e279ee625d12976e3d1d3b6d31c670e3
jayad <jp@gmail.com>
2024-05-28 13:27:59 +0530

    Merged remote changes

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -4,7 +4,6 @@ var execute = require("./conn.js");
 var rout = express.Router();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const cors = require("cors");
 
 // register admin
 rout.post("/register", async (req, res) => {
@@ -12,74 +11,96 @@ rout.post("/register", async (req, res) => {
   const sql = `INSERT INTO admin (username,email,password) VALUES ('${username}','${email}','${password}')`;
   const admin = await execute(sql);
   if (admin) {
-    return res.status(200).json({ message: "Registration successfull" });
+    return res
+      .status(200)
+      .json({ status: true, message: "Registration successfull" });
   } else {
-    return res.status(409).json({ message: "Registration Failed" });
+    return res
+      .status(409)
+      .json({ status: false, message: "Registration Failed" });
   }
 });
+
+// verify token middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ Success: false, message: "Token not found" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ Success: false, Message: "Invalid User" });
+    } else {
+      // req.email = decoded.email; // email fetch from token
+      next();
+    }
+  });
+};
 
 // login
 rout.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const sql = `SELECT * FROM admin WHERE email='${email}' AND password = '${password}'`;
-    const admin = await execute(sql);
-    if (admin.length <= 0) {
-      return res.status(401).json({ Success: false, Message: "Invalid User" });
+  const { email, password } = req.body;
+
+  const sql = `SELECT * FROM admin WHERE email='${email}' AND password = '${password}'`;
+
+  const admin = await execute(sql);
+
+  if (admin.length <= 0) {
+    return res.status(401).json({ Success: false, Message: "Invalid User" });
+  } else {
+    const payload = { email };
+    const secret = process.env.JWT_SECRET;
+
+    // generate token
+    // payload , secreat, expiresIn
+    const token = jwt.sign(payload, secret, {
+      expiresIn: "3h",
+    });
+
+    // Set a cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+    });
+
+    if (token) {
+      return res.json({ token, Success: true, Message: "Valid User" });
     } else {
-      const payload = { email };
-      const secret = process.env.JWT_SECRET;
-      // generate token
-      // payload , secreat, expiresIn
-      const token = jwt.sign(payload, secret, {
-        expiresIn: "3h",
-      });
-      //   store token in cookie
-      if (token) {
-        return res.json({ token, Success: true, Message: "Valid User" });
-      } else {
-        return res.json({ Success: false, Message: "Invalid User" });
-      }
+      return res.json({ Success: false, Message: "Invalid User" });
     }
-  } catch (error) {
-    return res.status(401).json({ Success: false, Message: error.message });
   }
 });
 
+// Logout
+rout.get("/logout", function (req, res) {
+  res.clearCookie("token");
+  return res.status(200).json({ Success: true, Message: "Logout Successfull" });
+});
+
 // Insert porject
-rout.post("/insert_project", async (req, res) => {
+rout.post("/insert-project", verifyToken, async (req, res) => {
   var d = req.body;
-  const sql = `INSERT INTO project(project_name,reason,type,division,category,priority,department,start_date,end_date,location,status,close_date) VALUES ('${d.project_name}','${d.reason}','${d.type}','${d.division}','${d.category}','${d.priority}','${d.department}','${d.start_date}','${d.end_date}','${d.location}','Registered','${d.end_date}')`;
+  const sql = `INSERT INTO project(project_name,reason,type,division,category,priority,department,start_date,end_date,location,status) VALUES ('${d.project_name}','${d.reason}','${d.type}','${d.division}','${d.category}','${d.priority}','${d.department}','${d.start_date}','${d.end_date}','${d.location}','Registered')`;
   const project = await execute(sql);
   return res.status(200).json({
     Status: true,
     Message: "New project added successfully",
+    project,
   });
 });
 
 // Get all projects
-rout.get("/get_projects", async (req, res) => {
+rout.get("/get-all-projects", verifyToken, async (req, res) => {
   const sql = `SELECT * FROM project ORDER BY project_id DESC`;
   const projects = await execute(sql);
   return res.status(200).json(projects);
 });
 
 // change status of project
-rout.post("/change_status", async (req, res) => {
+rout.post("/change-project-status", verifyToken, async (req, res) => {
   var d = req.body;
-  const date = new Date();
-
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  const currentDate =
-    year.toString() + "-" + month.toString() + "-" + day.toString();
-
-  if (d.status === "Closed") {
-    var sql = `UPDATE project SET status = '${d.status}' , close_date = '${currentDate}' WHERE project_id = '${d.id}'`;
-  } else {
-    var sql = `UPDATE project SET status = '${d.status}' WHERE project_id = '${d.id}'`;
-  }
+  var sql = `UPDATE project SET status = '${d.status}' WHERE project_id = '${d.id}'`;
   await execute(sql);
   res
     .status(200)
@@ -87,69 +108,61 @@ rout.post("/change_status", async (req, res) => {
 });
 
 // get project info like count , deparment and closure_delay
-rout.get("/get_project_count", async (req, res) => {
-  const sql = `SELECT * FROM project`;
-  const all_projects = await execute(sql);
-  // set initial value 0
-  var total_project_count = all_projects.length;
-  var running_project_count = 0;
-  var close_project_count = 0;
-  var cancel_project_count = 0;
+rout.get("/get-project-count", verifyToken, async (req, res) => {
+  const sql1 = `SELECT COUNT(*) as total_project_count, SUM(CASE WHEN status = 'Running' THEN 1 ELSE 0 END) AS running_project_count, SUM(CASE WHEN status = 'Closed' THEN 1 ELSE 0 END) AS close_project_count, SUM(CASE WHEN status = 'Cancelled' THEN 1 ELSE 0 END) AS cancel_project_count  from project;`;
 
-  const sql2 = `SELECT COUNT(*) AS closer_delay FROM project WHERE close_date > end_date`;
-  const closer_delay = await execute(sql2);
+  const projects = await execute(sql1);
 
-  const sql1 = `SELECT department, COUNT(*) AS totalProjects, SUM(CASE WHEN status='Closed' THEN 1 ELSE 0 END) AS closeProjects FROM project GROUP BY department`;
-  const departmen_info = await execute(sql1);
-  const project_data = departmen_info.map((closeProject) => {
-    var all_project_info = {
-      departments: closeProject.department,
-      totalProject: closeProject.totalProjects,
-      closeProject: closeProject.closeProjects,
-      closer_delay,
-    };
-    return all_project_info;
-  });
+  const total_project_count = projects[0].total_project_count;
+  const running_project_count = projects[0].running_project_count;
+  const close_project_count = projects[0].close_project_count;
+  const cancel_project_count = projects[0].cancel_project_count;
 
-  // project counting
-  for (var i = 0; i < all_projects.length; i++) {
-    if (all_projects[i].status === "Running") running_project_count += 1;
-    if (all_projects[i].status === "Closed") close_project_count += 1;
-    if (all_projects[i].status === "Cancelled") cancel_project_count += 1;
-  }
+  const sql2 = `SELECT COUNT(*) AS closer_delay FROM project WHERE status='Running' AND end_date < CURDATE()`;
+  const cd = await execute(sql2);
+
   if (total_project_count > 0) {
     res.status(200).json({
-      total_project_count,
-      running_project_count,
-      close_project_count,
-      cancel_project_count,
-      project_data,
+      total_project_count: total_project_count,
+      running_project_count: running_project_count,
+      close_project_count: close_project_count,
+      cancel_project_count: cancel_project_count,
+      closer_delay: cd[0].closer_delay,
     });
   } else {
     res.status(404).json({ Status: false, Message: "Projects not found" });
   }
 });
 
-// to verify token
-rout.post("/verify_token", async (req, res) => {
-  const { token } = req.body;
-  // verify token
-  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) {
-      return res.status(404).json({ Success: false, Message: "Invalid Token" });
-    }
-    const sql = `SELECT * FROM admin WHERE email = '${decoded.email}'`;
-    const admin = await execute(sql);
-    const username = admin.username;
-    if (admin.length > 0)
-      return res
-        .status(200)
-        .json({ Success: true, Message: "Valid Token", username });
-    else
-      return res
-        .status(404)
-        .json({ Success: false, Message: "Invalid Token", username });
+rout.get("/get-chart-data", verifyToken, async (req, res) => {
+  const sql = `SELECT department, COUNT(*) AS totalProjects,
+  SUM(CASE WHEN status='Closed' THEN 1 ELSE 0 END) AS closeProjects, (SUM (CASE WHEN status='Closed' THEN 1 ELSE 0 END) / COUNT(*)) * 100 AS percentage FROM project GROUP BY department`;
+
+  // unique departments , total_projects , close_projects
+  const departmentwise_info = await execute(sql);
+
+  const all_departments_data = departmentwise_info.map((project) => {
+    var all_project_info = {
+      departments: project.department,
+      totalProject: project.totalProjects,
+      closeProject: project.closeProjects,
+      percentage: Math.round(project.percentage),
+    };
+    return all_project_info;
   });
+
+  if (all_departments_data.length > 0) {
+    return res.status(200).json(all_departments_data);
+  } else {
+    return res.status(401).json(all_departments_data, "Projects not found");
+  }
+});
+
+rout.get("/get_departments_percentage", async (req, res) => {
+  const sql = `SELECT  department , (COUNT(*)) AS total_count FROM project GROUP BY department`;
+  const depts = await execute(sql);
+  res.send(depts);
+  // console.log(depts);
 });
 
 // export
--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const execute = require("./routes/conn.js");
 
 // Import route
 const adminRoute = require("./routes/adminRoute.js");
@@ -11,21 +10,23 @@ const adminRoute = require("./routes/adminRoute.js");
 // App
 const app = express();
 
-// Middlewares
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000/"],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-
-app.use(cors({ origin: true }));
+// middlewares
 app.use(express.json()); // Parse incoming JSON data
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 // routes
 app.use("/", adminRoute);
 
-app.listen(1000, () => console.log("Server listening on port 1000"));
+// PORT
+const PORT = 1000;
+
+// listening on port 3000
+app.listen(PORT, () => console.log("Server listening on port : " + PORT));
